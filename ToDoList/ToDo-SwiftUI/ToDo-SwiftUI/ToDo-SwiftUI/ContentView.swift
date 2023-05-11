504cec8bfcc9634af784e7688d4edf18df9e8eb3
Carol Bezerra <carol.arbe@gmail.com>
2023-05-11 17:12:22 -0300

    Add ToDoList folder

---
+++ ToDoList/ToDo-SwiftUI/ToDo-SwiftUI/ToDo-SwiftUI/ContentView.swift
@@ -0,0 +1,69 @@
+//
+//  ContentView.swift
+//  ToDo-SwiftUI
+//
+//  Created by Carol Bezerra on 30/04/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var username: String = ""
+
+    var body: some View {
+        NavigationView {
+            VStack {
+                HStack {
+
+                    TextField("", text: $username)
+                        .padding(5)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 200)
+                                .stroke(Color("darkviolet"), lineWidth: 2)
+                        )
+
+                    Button(role: nil) {
+                        print("pressed")
+                    } label: {
+                        Image(systemName: "plus.rectangle.fill").font(.system(size: 30))
+                    }
+                }
+                .padding([.top, .bottom], 50)
+                .padding([.trailing, .leading], 10)
+
+                Spacer()
+            }
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+
+                ToolbarItem(placement: .principal) {
+                    VStack {
+
+                        Text(" ")
+                        Divider()
+                            .frame(width: 200, height: 4)
+                            .overlay(Color("darksalmon"))
+
+                        Text("My To Do List")
+                            .font(
+                                .custom("Dancing Script", size: 30, relativeTo: .headline)
+                            )
+
+                        Text("Double-tap in a completed item")
+                            .font(
+                                .custom("Dancing Script", size: 16, relativeTo: .subheadline)
+                            )
+                            .foregroundColor(Color("darksalmon"))
+                    }
+                }
+            }
+
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ToDoList/ToDo-SwiftUI/ToDo-SwiftUI/ToDo-SwiftUI/ToDo_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDo_SwiftUIApp.swift
+//  ToDo-SwiftUI
+//
+//  Created by Carol Bezerra on 30/04/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDo_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
