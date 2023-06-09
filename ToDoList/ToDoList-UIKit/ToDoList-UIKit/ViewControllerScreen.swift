bf57184f09ab9e334f92bbfe48019eb3c1d9e62b
Carol Bezerra <carol.arbe@gmail.com>
2023-06-09 18:49:30 -0300

    Add UIView, backgroundImage and title

--- ToDoList/ToDoList-UIKit/ToDoList-UIKit/ViewControllerScreen.swift
+++ ToDoList/ToDoList-UIKit/ToDoList-UIKit/ViewControllerScreen.swift
@@ -9,6 +9,15 @@ import UIKit
 
 final class ViewControllerScreen: UIView {
 
+    lazy var mainView: UIView = {
+        let mainView = UIView()
+
+        mainView.backgroundColor = .white
+        mainView.translatesAutoresizingMaskIntoConstraints = false
+
+        return mainView
+    }()
+
     lazy var backgroundImage: UIImageView = {
         let backgroundImage = UIImageView(frame: CGRect(x: 0, y: 0, width: 0, height: 0))
         backgroundImage.image = UIImage(named: "ToDoList")
@@ -18,41 +27,61 @@ final class ViewControllerScreen: UIView {
         return backgroundImage
     }()
 
-//    lazy var title: UILabel = {
-//        let title = UILabel()
-//
-//        title.text = "My To Do List"
+    lazy var title: UILabel = {
+        let title = UILabel()
+
+        title.text = "My To Do List"
 //        title.font = UIFont(name: "DancingScript-Bold", size: 30)
-//
-//        return title
-//    }()
-//
-//    lazy var subtitle: UILabel = {
-//        let subtitle = UILabel()
-//
-//        subtitle.text = "Long press in a completed item"
-//        subtitle.textColor = UIColor(named: "darksalmon")
-//
-//        return subtitle
-//    }()
+        title.textColor = .red
+        title.textAlignment = .center
+
+        return title
+    }()
+
+    lazy var subtitle: UILabel = {
+        let subtitle = UILabel()
+
+        subtitle.text = "Long press in a completed item"
+        subtitle.textColor = UIColor(named: "darksalmon")
+
+        return subtitle
+    }()
 
     override init(frame: CGRect = .zero) {
         super.init(frame: frame)
 
-//        let backgroundImage = UIImage(named: "ToDoList")
-//
-//        backgroundColor = UIColor(patternImage: backgroundImage!)
+        // MARK: - Content
 
-        addSubview(backgroundImage)
+        addSubview(mainView)
+
+        mainView.topAnchor.constraint(equalTo: topAnchor).isActive = true
+        mainView.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
+        mainView.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
+        mainView.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
+
+        // MARK: - Background Image
+
+        mainView.addSubview(backgroundImage)
 
         backgroundImage.topAnchor.constraint(equalTo: topAnchor).isActive = true
         backgroundImage.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
         backgroundImage.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
         backgroundImage.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
+        backgroundImage.centerXAnchor.constraint(equalTo: mainView.centerXAnchor).isActive = true
 
-//        addSubview(title)
-//
-//        title.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor).isActive = true
+        // MARK: - Title
+
+        backgroundImage.addSubview(title)
+
+        title.topAnchor.constraint(equalTo: backgroundImage.topAnchor, constant: 50).isActive = true
+        title.leadingAnchor.constraint(equalTo: backgroundImage.leadingAnchor).isActive = true
+        title.widthAnchor.constraint(equalToConstant: 150).isActive = true
+        title.heightAnchor.constraint(equalToConstant: 150).isActive = true
+        title.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+
+        // MARK: - Subtitle
+
+
     }
 
     required init?(coder: NSCoder) {
